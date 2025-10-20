666dee8ce3d6d2b7eb18a38245881c1b46e23533
eightynine77 <theskyisgrey80@proton.me>
2025-10-20 07:01:44 +0700

    Add files via upload

---
+++ pushall.bat
@@ -0,0 +1,19 @@
+@echo off
+cls
+title push git
+gecho "<red>WARNING: this will commit ALL changes"
+echo.
+gecho "<yellow>if you want to selectively commit files/folders, don't use this batch script"
+echo.
+echo.
+pause
+git add -A
+type /p "commit_message=your commit message: "
+echo.
+git commit -m "%commit_message%"
+echo pushing all changes...
+echo.
+git push origin main
+echo.
+echo.
+echo pushing complete
