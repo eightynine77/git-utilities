35a5b67694eb607b82303eab3080da3622d2806a
eightynine77 <theskyisgrey80@proton.me>
2025-12-19 15:56:45 +0700

    added new arguments, --push and -p, for gitcommit.bat
    these arguments are both for pushing a commit to remote repo

--- gitcommit.bat
+++ gitcommit.bat
@@ -1,11 +1,32 @@
 @echo off
 rem gitutils
+
+if exist ".git\" (
+    if "%~1"=="--push" (
+        git push origin main
+        goto :end
+    ) else if "%~1"=="-p" (
+        git push origin main
+        goto :end
+    )
+)
+
 if "%~1"=="" (
     echo No arguments provided.
     echo.
-echo usage: gitcommit [your git commit message]
+echo usage
+echo ====================
+echo if you want to make a commit with a commit message, use: 
+echo gitcommit [your git commit message]
 echo.
 echo for example: gitcommit fixed the UI
+echo.
+echo.
+echo if you want to only make a commit and nothing else, use:
+echo gitcommit [-p ^| --push]
+echo.
+echo for example: gitcommit --push
+echo gitcommit -p
     goto :end
 )
 
@@ -13,6 +34,7 @@ if exist ".git\" (
 git add -A
 git commit -m "%*"
 echo.
+git pull origin main
 git push origin main
 echo.
 echo.
@@ -21,4 +43,5 @@ exit /b
 ) else (
 echo you are not in a github repo...
 )
+
 :end
