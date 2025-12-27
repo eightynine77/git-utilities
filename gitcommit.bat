c6de666d682fc28e7d22142e5e9450ba1cd50ab3
eightynine77 <theskyisgrey80@proton.me>
2025-12-27 14:14:41 +0700

    added "gitcommit -c" command
    for staging commits

--- gitcommit.bat
+++ gitcommit.bat
@@ -26,6 +26,24 @@ goto :end
 ) else if "%arg%"=="-h" (
 call :helpmsg
 goto :end
+) else if "%arg%"=="--comment" (
+git add -A
+git commit
+echo.
+echo.
+echo ==========================
+echo done adding all changes and adding a comment.
+echo ==========================
+goto :end
+) else if "%arg%"=="-c" (
+git add -A
+git commit
+echo.
+echo.
+echo ==========================
+echo done adding all changes and adding a comment.
+echo ==========================
+goto :end
 )
 
 if "%arg:~0,1%"=="-" (
@@ -54,20 +72,23 @@ echo usage
 echo ====================
 echo if you want to make a commit with a commit message, use: 
 echo gitcommit [your git commit message]
-echo.
 echo for example: gitcommit fixed the UI
 echo.
 echo if you want to only make a commit and nothing else, use:
 echo gitcommit [-ph ^| --push]
-echo.
 echo for example: gitcommit --push
-echo gitcommit -ph
 echo.
 echo if you want to pull from remote, use:
 echo gitcommit [-pl ^| --pull]
-echo.
 echo for example: gitcommit --pull
-echo gitcommit -pl
+echo.
+echo if you want to only stage stanges and add git comment, use:
+echo gitcommit [-c ^| --comment]
+echo for example: gitcommit --comment
+echo.
+echo NOTE: this "gitcommit -c" command will enter git's "git commit" mode that
+echo allows you to enter git commit message with description with your default
+echo text editor
 
 :helppopup
 echo type --help or -h to see the list of commands for this script
